9c0436cb4d362ff038ee14cdcb576d39862b75f9
aashitanegii <aashitanegii@gmail.com>
2026-07-08 14:05:29 +0530

    Completed Session 2 Assignment - Identity Echo Interface

--- Session-02-Identity-Echo-Interface/app.py
+++ Session-02-Identity-Echo-Interface/app.py
@@ -1,10 +1,16 @@
 import streamlit as st
+
+# Title
 st.title("The Identity Echo Interface")
+
+# Description
 st.write("Enter your name and message below.")
 
+# Inputs
 user_name = st.text_input("Enter your Name")
 user_message = st.text_input("Enter your Message")
 
+# Button
 if st.button("Transmit"):
 
     if user_name == "":
@@ -18,6 +24,7 @@ if st.button("Transmit"):
             f"Transmission successful! Greetings, {user_name}. We received your message: {user_message}"
         )
 
+        # Bonus Task
         token_count = len(user_message) / 4
 
         st.info(
